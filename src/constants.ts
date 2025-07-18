c5d2d6e0a2903ddf062c960907edd82a05de0666
Yuji Nakamura <ny5117@icloud.com>
2025-07-18 10:56:40 +0800

    remove ""

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-export const TEMPLATE_DIR = path.join(process.cwd(), '"templates"');
+export const TEMPLATE_DIR = path.join(process.cwd(), "templates");
 export const ARTICLES_DIR = path.join(process.cwd(), "articles");
 export const MARKDOWN_EXT = ".md";
 export const TEMPLATE_ARG_PREFIX = "--template=";
